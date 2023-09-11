cca062223f8e7da1f06afb6663255bda42dfbc5b
Mathwebb <matheus.webber@hotmail.com>
2023-09-11 11:47:38 -0300

    Adding the file system handling and importing practices

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs').promises;
+const path = require("path");
+
 function fibonacci(n){
     let n1 = 0, n2 = 1, sum = 0;
 
@@ -10,6 +13,37 @@ function fibonacci(n){
     return n === 0 ? n1 : n2;
 }
 
-const result = fibonacci(5);
+async function findSalesFiles(folderName){
+    salesFiles = []
 
-console.log(result);
+    async function findFiles(folderName){
+        const items = await fs.readdir(folderName, { withFileTypes: true });
+
+        for(item of items){
+            itemPath = path.join(folderName, item.name);
+            if(item.isDirectory()){
+                await findFiles(itemPath);
+            } else {
+                if (path.extname(item.name) === ".json") {
+                    await salesFiles.push(itemPath);
+                }
+            }
+        }
+    }
+    await findFiles(folderName);
+    return salesFiles;
+}
+
+async function main(){
+    const result = fibonacci(5);
+    console.log(result);
+
+    const files = await fs.readdir(__dirname);
+    console.log(files);
+
+    const salesDir = path.join(__dirname, '..', 'stores');
+    const salesFiles = await findSalesFiles(salesDir);
+    console.log(salesFiles);
+};
+
+main();
--- src/utils/adress-parser.js
+++ src/utils/adress-parser.js
@@ -1,4 +1,4 @@
-exports.parse = function parseOrder(order) {
+module.exports.parse = function parseOrder(order) {
     const match = order.match(/order:\s(?<order>\w+\s\w+).*address:\s(?<address>\w+\s\w+\s\w+).*payment info:\s(?<payment>\w+)/)
     return match.groups;
   }
